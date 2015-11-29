fc4ab129fc42f2874e3cbf7841634a8414e68077
steven-supersolid <steven@supersolid.com>
2015-11-29 16:39:04 +0000

    fix test

--- test/endtoend/making_a_post.spec.js
+++ test/endtoend/making_a_post.spec.js
@@ -1,10 +1,7 @@
 describe('making a post', function() {
 	it('logs in and creates a new post', function() {
 		browser.get('http://localhost:3001');
-		var navLogin = element(by.css('nav.login'));
-		console.log(navLogin.isDisplayed() === true);
-		navLogin.click();
-//		element(by.css('nav.login')).click();
+		element(by.css('nav .login')).click();
 		// fill out and submit login form
 		// submit a new post on the posts page
 		// the user sohuld now see their post as the first post on the page
