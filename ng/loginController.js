578a88e8b96b787a21c9d126251dbdec28392a6c
steven-supersolid <steven@supersolid.com>
2015-11-29 17:53:31 +0000

    complete end to end test

--- ng/loginController.js
+++ ng/loginController.js
@@ -1,8 +1,9 @@
-angular.module('app').controller('LoginController', function($scope, UserService) {
+angular.module('app').controller('LoginController', function($scope, UserService, $location) {
 	$scope.login = function(username, password) {
 		UserService.login(username, password)
 		.then(function(response) {
 			$scope.$emit('login', response.data);
+			$location.path('/');
 		});
 	};
 });
--- test/endtoend/making_a_post.spec.js
+++ test/endtoend/making_a_post.spec.js
@@ -1,9 +1,21 @@
+var chai = require('chai');
+chai.use(require('chai-as-promised'));
+var expect = chai.expect;
+
 describe('making a post', function() {
 	it('logs in and creates a new post', function() {
 		browser.get('http://localhost:3001');
+
 		element(by.css('nav .login')).click();
-		// fill out and submit login form
-		// submit a new post on the posts page
-		// the user sohuld now see their post as the first post on the page
+
+		element(by.model('username')).sendKeys('steven');
+		element(by.model('password')).sendKeys('word');
+		element(by.css('form .btn')).click();
+
+		var post = 'new post' + Math.random();
+		element(by.model('postBody')).sendKeys(post);
+		element(by.css('form .btn')).click();
+
+		expect(element.all(by.css('ul.list-group li')).first().getText()).to.eventually.contain(post);
 	});
 });
