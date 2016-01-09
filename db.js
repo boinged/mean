9397aa74c37208455b423e56685d6f4ff059c8d6
steven-supersolid <steven@supersolid.com>
2016-01-09 16:49:20 +0000

    debug

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 
 var url = process.env.MONGOLAB_URL || 'mongodb://localhost/socialapp';
+console.log('url ' + url);
 mongoose.connect(url);
 module.exports = mongoose;
