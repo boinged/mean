01e77428c134217be526cd8437c4c895d577e1db
steven-supersolid <steven@supersolid.com>
2016-01-09 16:57:26 +0000

    fix mongolab uri

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var url = process.env.MONGOLAB_URL || 'mongodb://localhost/socialapp';
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost/socialapp';
 console.log('url ' + url);
 mongoose.connect(url);
 module.exports = mongoose;
